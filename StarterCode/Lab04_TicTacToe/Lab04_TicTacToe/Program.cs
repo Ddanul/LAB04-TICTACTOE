50ac5fa1a5d53490a5b0df2be95130ab71798197
Danul De Leon <danul.deleon@gmail.com>
2018-10-12 09:55:18 -0700

    Instantiated Players and Game

--- StarterCode/Lab04_TicTacToe/Lab04_TicTacToe/Program.cs
+++ StarterCode/Lab04_TicTacToe/Lab04_TicTacToe/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Lab04_TicTacToe.Classes;
 
 namespace Lab04_TicTacToe
 {
@@ -12,6 +13,24 @@ namespace Lab04_TicTacToe
 		static void PlayGame()
 		{
 			// TODO: Instantiate your players
+            Console.Write("Please enter name for Player 1: ");
+            string playerOneName = Console.ReadLine();
+            Console.Write("Please enter name for Player 2: ");
+            string playerTwoName = Console.ReadLine();
+            
+            Player p1 = new Player
+            {
+                Name = playerOneName,
+                Marker = "X"
+            };
+
+            Player p2 = new Player
+            {
+                Name = playerTwoName,
+                Marker = "O"
+            };
+
+            Game newGame = new Game(p1, p2);
 			// Create the Game
 			// Play the Game
 			// Output the winner
